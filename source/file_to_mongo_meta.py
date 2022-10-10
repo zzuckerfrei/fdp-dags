8e7d5b1a77a86e31d121f4169c7cf08f9acd8689
smlee <seonmin1219@gmail.com>
2022-10-10 23:12:24 +0900

    insert meta one

--- source/file_to_mongo_meta.py
+++ source/file_to_mongo_meta.py
@@ -5,8 +5,10 @@ from time import sleep
 import pendulum
 import random
 
+import psycopg2.extras
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.bash import BashOperator
 from airflow.models.variable import Variable
@@ -21,7 +23,6 @@ from airflow.exceptions import AirflowException
     start_date=pendulum.datetime(2022, 9, 29, tz="UTC"),
 )
 def FileToMongo():
-
     check_schema_exists = PostgresOperator(
         task_id="create_schema_meta",
         postgres_conn_id="fdp_meta_pg_conn",
@@ -74,7 +75,57 @@ def FileToMongo():
             # logging.error(e)
             # return 1
 
-    check_schema_exists >> create_table_meta_meta >> create_table_meta_competition >> create_table_meta_match >> create_table_meta_lineup >> create_table_meta_event
+    @task.python
+    def get_meta_one():
+        try:
+            url = ""
+            data_type = ""
+            res = requests.get(
+                "http://172.18.0.3:8000/api/v1/meta/read_one/{}".format("match")).json()  # test (o), 172.18.0.3(o)
+
+            logging.info("get meta one ... {}".format(res))
+            logging.info("date_type ... {}".format(res["result"]["data_type"]))  ## date_type
+            logging.info("count_in_dir ... {}".format(res["result"]["count_in_dir"]))  ## count_in_dir
+            logging.info("list_in_dir ... {}".format(res["result"]["list_in_dir"]))  ## list_in_dir
+
+            return res
+
+        except Exception as e:
+            raise AirflowException(e)
+
+    @task.python
+    def insert_meta_one(res: dict):
+        try:
+            date_type = res["result"]["data_type"]
+            count_in_dir = res["result"]["count_in_dir"]
+            list_in_dir = res["result"]["list_in_dir"]
+
+            postgres_hook = PostgresHook(postgres_conn_id="fdp_meta_pg_conn")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            # fdp_meta
+            sql = "INSERT INTO meta.fdp_meta(data_type, count_in_dir, count_in_db, reg_date) VALUES('%s','%s','%s',%s)" % (
+                date_type,
+                count_in_dir,
+                "0",
+                "default"
+            )
+            cur.execute(sql)
+
+            # fdp_item
+
+            sql = f'INSERT INTO meta.fdp_{date_type} (data_type, file_path, mongo_flag) VALUES %s'
+            argslist = [(date_type, i, "N") for i in list_in_dir]
+
+            psycopg2.extras.execute_values(cur=cur, sql=sql, argslist=argslist)
+
+            conn.commit()
+
+        except Exception as e:
+            raise AirflowException(e)
+
+    # check_schema_exists >> create_table_meta_meta >> create_table_meta_competition >> create_table_meta_match >> create_table_meta_lineup >> create_table_meta_event >> \
+    insert_meta_one(get_meta_one())
     # get_meta()
 
 
