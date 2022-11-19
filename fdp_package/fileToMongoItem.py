55b8461faf8c8254c380380003c50d4ddec16616
smlee <seonmin1219@gmail.com>
2022-11-19 22:35:27 +0900

    create_item 호출시 적재는 정상이지만, 메타 업데이트가 비정상적으로 종료되는 경우에 대한 최신화 로직 추가

--- fdp_package/fileToMongoItem.py
+++ fdp_package/fileToMongoItem.py
@@ -45,6 +45,19 @@ def create_item(data_type: str, file_path: str):
         # 복잡한 mongodb 트랜잭션 대신 한 번 select해서 존재여부 체크하고 없을 때만 적재 할까?
         check = requests.get(url=f"{url_item_read_one}", params=params).json()
         if check["result"] is not None:
+            # 여기까지 왔다는 것은 비정상적인 상태라는 뜻. fdp_item 테이블의 갑을 N -> Y로 변경시켜야 정상적인 상황임
+            postgres_hook = PostgresHook(postgres_conn_id="fdp_meta_pg_conn")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            cur.execute("BEGIN;")
+
+            with open(Variable.get("sql_base_dir") + "item/update_table_item.sql", "r") as f:
+                query = f.read()
+            query_item = query.format(data_type=data_type, file_path=file_path)
+            logging.info("query_item is ...{}".format(query_item))
+            cur.execute(query_item)
+            cur.execute("COMMIT;")
+            logging.warn(f"{file_path}에 대한 META 테이블 최신화 적용")
             raise FileExistsError(f"create_item :: {file_path} is already in MongoDB")
 
         res = requests.post(url=f"{url_item_create}", params=params).json()
